08e9c1ee5993b386524ca17d36347e3cd078fb5e
Chris <hurdca1993@gmail.com>
2022-06-14 20:49:55 -0400

    Added SalesReport.java

---
+++ src/main/java/com/techelevator/SalesReport.java
@@ -0,0 +1,33 @@
+package com.techelevator;
+
+import com.techelevator.products.VendingMachineItem;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SalesReport {
+    private Map<String, Integer> salesReport = new HashMap<>();
+
+    public SalesReport(File inputFile, Scanner scanner) {
+
+    }
+
+    public Map<String, Integer> getSalesReport() {
+        return salesReport;
+    }
+
+    public void updateSalesReport(VendingMachineItem item) {
+
+    }
+
+    private void sortSalesReport() {
+
+    }
+
+    public void writeReportToFile(File outputFile) {
+
+    }
+
+}
