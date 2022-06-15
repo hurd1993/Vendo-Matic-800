6ca913d48900e04809a4b974d81f16ab55236b4e
Chris <hurdca1993@gmail.com>
2022-06-14 20:27:45 -0400

    Fully implemented enums

--- src/main/java/com/techelevator/constants/BillAmounts.java
+++ src/main/java/com/techelevator/constants/BillAmounts.java
@@ -1,4 +1,18 @@
 package com.techelevator.constants;
 
 public enum BillAmounts {
+    ONE(1),
+    TWO(2),
+    FIVE(5),
+    TEN(10);
+
+    private int value;
+
+    BillAmounts(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
--- src/main/java/com/techelevator/constants/CoinAmounts.java
+++ src/main/java/com/techelevator/constants/CoinAmounts.java
@@ -1,4 +1,17 @@
 package com.techelevator.constants;
 
 public enum CoinAmounts {
+    NICKEL(.05),
+    DIME(.10),
+    QUARTER(.25);
+
+    private double value;
+
+    CoinAmounts(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
