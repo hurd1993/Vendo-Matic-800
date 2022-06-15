18355fe73b39c2efe16ddaa0388e4b8062dd5455
Chris <hurdca1993@gmail.com>
2022-06-14 20:29:32 -0400

    Fully implemented AuditLog.java

--- src/main/java/com/techelevator/AuditLog.java
+++ src/main/java/com/techelevator/AuditLog.java
@@ -1,4 +1,24 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class AuditLog {
+    public static void log(String message) {
+        File auditFile = new File("Log.txt");
+
+        String pattern = "MM/dd/yyyy hh:mm:ss a";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        LocalDateTime now = LocalDateTime.now();
+        String date = formatter.format(now);
+        try(PrintWriter writer = new PrintWriter(new FileOutputStream(auditFile,true))) {
+            writer.println(date  + " " + message);
+        } catch (FileNotFoundException e) {
+            System.out.println("***" + e.getMessage() + "***");
+        }
+    }
 }
